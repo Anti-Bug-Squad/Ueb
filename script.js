d5d18ee2c7d200eafa0d1eddca24021a250d5ece
Dhairya <joshidhairaya2002@gmail.com>
2021-05-28 20:29:25 +0530

    minor change

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ $(document).ready(
         $(".dbutton").mousedown(function(){
 
             $(this).css("box-shadow","-1px -1px black");
+            $(this).css("font-size","1rem");
+
         });
         $(".dbutton").mouseup(function(){
 
             $(this).css("box-shadow","2px 2px black");
-        })
+            $(this).css("font-size","1.1rem");
+        });
+        $(".dbutton").mouseleave(function(){
+
+            $(this).css("box-shadow","2px 2px black");
+            $(this).css("font-size","1.1rem");
+        });
     }
 
 
-)
+);
